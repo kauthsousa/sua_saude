b460ff847848acd5a42239188e5da69f471e3ba7
Giovana Carneiro <pessoal.giolopes@gmail.com>
2024-11-04 15:45:55 -0300

    criação da página de login

--- src/app/components/menusuperior_deslogado.tsx
+++ src/app/components/menusuperior_deslogado.tsx
@@ -9,10 +9,10 @@ export default function MenuDeslogado() {
             <Image src="/images/logodeitada.png" className={styles.imagem_logo} alt="Sua Saúde logo" width={400} height={50} priority />
             <nav className={styles.menu_opcoes} aria-label="Menu superior">
                 <ul className={styles.deslogado}>
-                    <li><Link href="../user/home" aria-label="Ir para Seja um parceiro">Seja um Parceiro</Link></li>
-                    <li><Link href="../user/produto" aria-label="Ir para Fale Conosco">Fale Conosco</Link></li>
-                    <li><Link href="../user/vendas" aria-label="Ir para Cadastre-se">Cadastre-se</Link></li>
-                    <li><Link href="../user/login" aria-label="Ir para Login">Entrar</Link></li>
+                    <li><Link href="../pages/sejaparceiro" aria-label="Ir para Seja um parceiro">Seja um Parceiro</Link></li>
+                    <li><Link href="../pages/faleconosco" aria-label="Ir para Fale Conosco">Fale Conosco</Link></li>
+                    <li><Link href="../pages/cadastrouser" aria-label="Ir para Cadastre-se">Cadastre-se</Link></li>
+                    <Link href="../pages/login" aria-label="Ir para Login"><li>Entrar</li></Link>
                 </ul>
             </nav>
         </div>
---
+++ src/app/pages/login/page.tsx
@@ -0,0 +1,61 @@
+"use client"
+import React from "react";
+import styles from "../../page.module.css";
+import MenuDeslogado from "../../components/menusuperior_deslogado";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Login() {
+
+    return (
+        <div className={styles.pagina_cadastro} role="main">
+            <header aria-label="Menu Principal">
+                <MenuDeslogado />
+            </header>
+            <main className={`${styles.main} ${styles.telaCadastro}`}>
+                <Image
+                    src="/images/ÍconesCadastro.png"
+                    className={styles.imgCadastro}
+                    alt="Ícones representativos"
+                    width={500}
+                    height={300}
+                    priority
+                />
+                <div className={`${styles.modalCadastro} ${styles.login}`} role="form" aria-labelledby="tituloCadastro">
+                    <span id="tituloCadastro" className={styles.titulo_modalCadastro}>Login</span>
+                    <div className={styles.camposCadastro}>
+                        <form>
+                            <label htmlFor="email" aria-hidden="true" hidden>Email</label>
+                            <input
+                                type="text"
+                                id="email"
+                                placeholder="Email"
+                                maxLength={100}
+                                className={`${styles.geral} ${styles.codProf} ${styles.inputLogin}`}
+                                required
+                                aria-required="true"
+                                aria-label="Email"
+                            />
+                            <label htmlFor="Senha" aria-hidden="true" hidden>Senha</label>
+                            <input
+                                type="password"
+                                id="senha"
+                                placeholder="Senha"
+                                maxLength={50}
+                                className={`${styles.geral} ${styles.codProf} ${styles.inputLogin}`}
+                                required
+                                aria-required="true"
+                                aria-label="Senha"
+                            />
+                            <Link href="recuperarconta" className={`${styles.esqueceuSenha} ${styles.hoverLink}`}>Esqueceu a senha? Recuperar conta</Link>
+                            <button type="submit">Entrar</button>
+                            <Link href="cadastrouser" className={`${styles.semConta} ${styles.hoverLink}`}>Ainda não possui conta? Cadastre-se</Link>
+                        </form>
+                    </div>
+                </div>
+            </main>
+            <footer className={styles.footer} aria-label="Rodapé">
+            </footer>
+        </div>
+    );
+}
