19a29ddacdd13c65252a01ff2eedbf3c3f40c67d
Giovana Carneiro <pessoal.giolopes@gmail.com>
2024-11-25 16:45:45 -0300

    iniciando a criação do modal novo dependente

---
+++ src/app/components/novoDependente.tsx
@@ -0,0 +1,65 @@
+"use client";
+import React, {useState} from "react";
+import Image from "next/image";
+import styles from "../page.module.css";
+import InputMask from "react-input-mask";
+
+interface NovoDependenteProps {
+    onClose: () => void; // Define o tipo da função de fechar o modal
+}
+
+export default function NovoDependente({ onClose }: NovoDependenteProps) {
+    const [inputType, setInputType] = useState<string>('text');
+    const [dataNascimento, setDataNascimento] = useState<string>("");
+    const [dataVisualizada, setDataVisualizada] = useState<string>("");
+
+    const formatarData = (data: string): string => {
+        if (!data) return "";
+        const [ano, mes, dia] = data.split('-');
+        return `${dia}/${mes}/${ano}`;
+      };
+    
+      const handleFocus = () => {
+        setInputType('date');
+        setDataVisualizada(dataNascimento); // Para mostrar a data ISO ao abrir o calendário
+      };
+    
+      const handleBlur = () => {
+        setInputType('text');
+        setDataVisualizada(formatarData(dataNascimento)); // Formata para dd/mm/aaaa ao sair do campo
+      };
+
+      const handleDataChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const valor = e.target.value;
+        setDataNascimento(valor);
+        setDataVisualizada(valor);
+      };
+
+    return(
+        <div className={styles.modalOverlay}>    
+            <div className={`${styles.modalCadastro} ${styles.novoDep}`} role="form" aria-labelledby="nomeDependente">
+                <span id="nomeDependente" className={`${styles.titulo_modalCadastro} ${styles.titulo_novoDep}`}>Daniel Soares</span>
+                <Image src="/image/close.png" alt="Botão de Fechar" className={styles.btnCloseDep} width={200} height={200} priority/>
+                <form>
+                    <div className={styles.dados_novoDep}>
+                        <input type="text" placeholder="Nome Completo" className={styles.geral}/>
+                        <input
+                            type={inputType}
+                            value={inputType === 'text' ? dataVisualizada : dataNascimento}
+                            onChange={handleDataChange}
+                            placeholder="Data de nascimento"
+                            onFocus={handleFocus}
+                            onBlur={handleBlur}
+                            className={`${styles.pequeno} ${styles.geralC} ${styles.data} ${inputType === 'date' ? styles.dateInput : ''} ${styles.dataDep}`}
+                            required
+                            aria-required="true"
+                            aria-label="Data de Nascimento"
+                        />
+                        <InputMask mask="999.999.999-99" placeholder="CPF" className={`${styles.geral} ${styles.intermediarioC} ${styles.cpfDep}`} required aria-required="true" aria-label="CPF"/>
+                    </div>
+                    <button className={styles.btnNovoDep}>Cadastrar</button>
+                </form>
+            </div>
+        </div>
+    );
+}
--- src/app/pages/dependentes/page.tsx
+++ src/app/pages/dependentes/page.tsx
@@ -3,16 +3,22 @@ import React, { useState } from "react";
 import styles from "../../page.module.css";
 import MenuLogado from "../../components/menusuperior_logado";
 import Image from "next/image";
-import Link from "next/link";
+import NovoDependente from "@/app/components/novoDependente";
 
 export default function Dependentes() {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
     return(
         <div className={styles.pagina_cadastro} role="main">
             <header aria-label="Menu Principal"><MenuLogado/></header>
             <main className={`${styles.main} ${styles.telaConfiguracoes} ${styles.telaDependentes}`}>
                 <div className={styles.topoDependentes}>    
                     <span className={styles.titulo_modalConfiguracoes}>Dependentes</span>  
-                    <button className={styles.btnDependentes}><Image className={styles.addDep} src="/images/add.png" alt="" width={100} height={100} priority/>NOVO DEPENDENTE</button> 
+                    <button className={styles.btnDependentes} onClick={openModal}><Image className={styles.addDep} src="/images/add.png" alt="" width={100} height={100} priority/>NOVO DEPENDENTE</button> 
                 </div>
                 <div className={styles.dadosDependente}>
                     <div className={styles.info}>
@@ -26,6 +32,12 @@ export default function Dependentes() {
                     <Image src="/images/lixeira.svg" className={styles.lixeira} alt="Lixeira" width={100} height={100} priority/>
                 </div>
             </main>
+
+
+            {isModalOpen && (
+                <NovoDependente onClose={closeModal} /> // Passa a função `closeModal` como prop
+            )}
+
             <footer className={styles.footer} aria-label="Rodapé"></footer>
         </div>
     );
