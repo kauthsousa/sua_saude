2b138cd4f7bad07d1f00642c36514bd5cee0fdf0
Artur M R Dutra <artur@artzKali.local>
2024-11-26 15:20:50 -0300

    ajustes completos na página de contato/feedback

--- src/app/pages/faleconosco/page.tsx
+++ src/app/pages/faleconosco/page.tsx
@@ -12,21 +12,22 @@ export default function Home() {
         <div className={styles.page}>
             <header><MenuDeslogado /></header>
             <main className={styles.telaCadastro}>
-                <div>
-                    <div className={styles.chamadaContato}>
-                        <h1>Fale Conosco</h1>
-                        <h3>Entre em contato conosco e tire suas dúvidas. Teremos prazer em te atender! Sua saúde é a nossa prioridade</h3>
-                    </div>
-                    <div className={styles.modal}>
-                        <input type="text" placeholder="Nome Completo" />
-                        <input type="text" placeholder="E-mail" />
-                        <input type="text" placeholder="Telefone" />
-                        <input type="text" placeholder="Assunto" />
-                        <textarea placeholder="Descrição" />
-                        <button>Enviar Feedback</button>
-                    </div>
+                <div className={styles.chamadaContato}>
+                    <h1>Fale Conosco</h1>
+                    <h3>
+                        Entre em contato conosco e tire suas dúvidas. Teremos prazer em te atender! Sua saúde é a nossa prioridade.
+                    </h3>
+                </div>
+                <div className={styles.modal}>
+                    <input type="text" placeholder="Nome Completo" />
+                    <input type="text" placeholder="E-mail" />
+                    <input type="text" placeholder="Telefone" />
+                    <input type="text" placeholder="Assunto" />
+                    <textarea placeholder="Descrição" />
+                    <button>Entre em contato</button>
                 </div>
             </main>
+
             <Rodape></Rodape>
         </div>
     );
