8f83682a5fff752bb3a1a9a17e4b38325645a347
Artur M R Dutra <artur@artzKali.local>
2024-11-27 07:49:33 -0300

    ajustes na pagina de contato

--- src/app/components/menusuperior_deslogado.tsx
+++ src/app/components/menusuperior_deslogado.tsx
@@ -9,8 +9,8 @@ export default function MenuDeslogado() {
             <Image src="/images/logodeitada.png" className={styles.imagem_logo} alt="Sua Saúde logo" width={400} height={50} priority />
             <nav className={styles.menu_opcoes} aria-label="Menu superior">
                 <ul className={styles.deslogado}>
-                    <li><Link href="../user/home" aria-label="Ir para Seja um parceiro">Seja um Parceiro</Link></li>
-                    <li><Link href="../user/produto" aria-label="Ir para Fale Conosco">Fale Conosco</Link></li>
+                    <li><Link href="../" aria-label="Ir para Seja um parceiro">Seja um Parceiro</Link></li>
+                    <li><Link href="../pages/faleconosco" aria-label="Ir para Fale Conosco">Fale Conosco</Link></li>
                     <li><Link href="../user/vendas" aria-label="Ir para Cadastre-se">Cadastre-se</Link></li>
                     <li><Link href="../user/login" aria-label="Ir para Login">Entrar</Link></li>
                 </ul>
