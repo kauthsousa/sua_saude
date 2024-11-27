7caffe4b9f670e32db3c22b4feb4e5659f578f13
Artur Dutra <arturmateusdutrabck@gmail.com>
2024-11-27 17:24:14 -0300

    Ajustando cardprofissional

---
+++ src/app/pages/pesquisaexterno/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../../page.module.css";
+import MenuDeslogado from "../../components/menusuperior_deslogado";
+import Carousel from "../../components/carrosel_especialidades";
+import Rodape from "../../components/rodape";
+import Mapa from '../../components/mapa';
+import Pesquisa from "../../components/inputs_pesquisa";
+import CardProfissional from "../../components/cardprofissional";
+
+export default function Home() {
+
+    const profissionais = [
+        {
+            id: 1,
+            nome: "Ana Moreira",
+            especialidade: "Nutricionista Clínica e Esportiva",
+            endereco: "Rua dos Prazeres, 18, Centro - Cidade do Norte - CE",
+            avaliacoes: 12,
+            distancia: "9 km",
+            imagem: "/images/proFavorito.png",
+        },
+        {
+            id: 2,
+            nome: "Carlos Oliveira",
+            especialidade: "Fisioterapeuta",
+            endereco: "Avenida Central, 45 - Bairro Sul - Cidade do Norte - CE",
+            avaliacoes: 20,
+            distancia: "5 km",
+            imagem: "/images/proFavorito2.png",
+        },
+    ];
+
+    return (
+        <div className={styles.page}>
+            <header><MenuDeslogado /></header>
+            <main className={styles.main}>
+
+                {/* BANNER TOPO */}
+                <div className={styles.banner_pesquisa}>
+                    <Image className={styles.top} src="/images/top.png" alt={""} layout="responsive" width={1500} height={300} />
+                    <Image className={styles.icone_central} src="/images/locale.png" alt={""} width={80} height={80} />
+
+                    {/* INPUTS DE PESQUISA */}
+                    <Pesquisa />
+
+                    {/* LISTA PROFISSIONAIS*/}
+                    <div className={styles.listaprofissionais}>
+                        <CardProfissional profissionais={profissionais} />
+                    </div>
+
+                    {/* ESPECIALIDADES */}
+                    <div>
+                        <Carousel />
+                    </div>
+
+                    {/* LOCALIZAÇÃO */}
+                    <div className={styles.mapa}>
+                        <Mapa />
+                    </div>
+                </div>
+            </main>
+
+
+            <Rodape></Rodape>
+        </div>
+    );
+}
