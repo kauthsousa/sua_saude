57e0e6a0d5790bd08641d8725eb7855486ff5fb4
Giovana Carneiro <pessoal.giolopes@gmail.com>
2024-11-28 16:03:54 -0300

    adicionando parâmetros no componente listaFavoritos

--- src/app/pages/favoritos/page.tsx
+++ src/app/pages/favoritos/page.tsx
@@ -6,6 +6,18 @@ import ListaFavoritos from "@/app/components/listafavoritos";
 
 export default function Favoritos() {
 
+    const profissional = {
+        nome: "Ana Moreira - Nutricionista Clínica e Esportiva",
+        endereco: "Rua dos Prazeres, 18, Centro - Cidade do Norte - CE",
+        avaliacoes: 12,
+        distancia: "9 km",
+        imagem: "/images/proFavorito.png",
+      };
+    
+      const handleAgendarConsulta = (profissional: any) => {
+        alert(`Você clicou em Agendar Consulta para: ${profissional.nome}`);
+      };
+
     return (
         <div className={styles.pagina_cadastro} role="main">
             <header aria-label="Menu Principal">
@@ -14,7 +26,7 @@ export default function Favoritos() {
             <main className={`${styles.main} ${styles.telaConfiguracoes}`}>
                 <div className={styles.telaFavoritos} role="imagem">
                     <span className={`${styles.titulo_modalConfiguracoes} ${styles.tituloFav}`}>Favoritos</span>   
-                    <div className={styles.containerFav}><ListaFavoritos/></div>
+                    <div className={styles.containerFav}><ListaFavoritos profissional={profissional} botaoTexto="Agendar Consulta" onBotaoClick={handleAgendarConsulta}/></div>{/* no onBotãoClick você coloca para chamar o modal, ao invés desse handleAgendarConsulta */}
                 </div>
             </main>
             <footer className={styles.footer} aria-label="Rodapé">
